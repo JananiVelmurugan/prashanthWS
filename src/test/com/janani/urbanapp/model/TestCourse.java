7e0d6b47376cfe0aa15ef16c27cf89703bc8285d
Janani <jananise@gmail.com>
2017-02-09 21:50:19 +0530

    Initial Commit

---
+++ src/test/com/janani/urbanapp/model/TestCourse.java
@@ -0,0 +1,24 @@
+package test.com.janani.urbanapp.model;
+
+import com.janani.urbanapp.model.Course;
+
+public class TestCourse {
+	public static void main(String[] args) {
+
+		Course course = new Course();
+
+		System.out.println(course);
+		//System.out.println(course.toString());
+
+		// System.out.println("Before Assigning Id : " + course.getId());
+		course.setId(1);
+		System.out.println("After Assigning Id : " + course.getId());
+
+		// System.out.println("Before Assigning Name : " + course.getName());
+		course.setName("Java");
+		System.out.println("After Assigning Id : " + course.getName());
+		
+		System.out.println(course);
+
+	}
+}
